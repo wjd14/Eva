97e2db4fbed63e830b19256cf8d5d476c2bba2d0
wjd14 <will@ayerdoyles.org>
2022-03-11 14:45:40 -0500

    Other changes before pizza

--- Eva/Eva/src/main/java/frc/robot/commands/TeleOpMoveArm.java
+++ Eva/Eva/src/main/java/frc/robot/commands/TeleOpMoveArm.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import frc.robot.subsystems.*;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.commands.MoveArm;
 import frc.robot.commands.TeleOpMoveArm;
 import frc.robot.RobotContainer;
 
